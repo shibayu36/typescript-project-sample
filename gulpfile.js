ceec857937f7c63e9209017187a7774b42c00496
shibayu36 <shibayu36@gmail.com>
2016-03-24 08:04:35 +0900

    remote useless

--- gulpfile.js
+++ gulpfile.js
@@ -26,20 +26,6 @@ gulp.task('build-ts', function () {
         .pipe(gulp.dest('./static/js'));
 });
 
-gulp.task('build-js-from-ts', function () {
-    return browserify({
-        entries: './src/ts/app.ts'
-    }).plugin('tsify')
-        .plugin('licensify')
-        .bundle()
-        .pipe(source('app.js'))
-        .pipe(buffer())
-        .pipe(uglify({
-            preserveComments: 'license'
-        }))
-        .pipe(gulp.dest('./static/js'));
-});
-
 gulp.task('watch', function () {
     gulp.watch('src/ts/**/*.ts', ['build-ts']);
 });
