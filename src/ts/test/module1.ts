6f400778b21a84f5466a27a7f1e11deea2775ce3
shibayu36 <shibayu36@gmail.com>
2016-03-29 19:27:04 +0900

    karmaの設定をしてテストが動くように

---
+++ src/ts/test/module1.ts
@@ -0,0 +1,12 @@
+/// <reference path="../../../typings/browser.d.ts" />
+
+"use strict";
+
+import { assert } from "chai";
+import module1 from "../module1";
+
+describe("module1 default function", () => {
+    it("returns module1", () => {
+        assert.equal(module1(), "module1");
+    });
+});
