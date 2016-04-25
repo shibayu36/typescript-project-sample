8db769d90708b956439436126b2269a4e16aa78c
shibayu36 <shibayu36@gmail.com>
2016-04-25 09:43:06 +0900

    fix

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -5,7 +5,9 @@
 import * as $ from "jquery";
 import * as _ from "lodash";
 import module1 from "./module1";
+import module2 from "./module2";
 
 $().ready(() => {
     alert(_.repeat(module1(), 3));
+    alert(_.repeat(module2(), 2));
 });
